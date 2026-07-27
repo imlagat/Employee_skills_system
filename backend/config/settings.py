3f3c127363a29575918bd5ce053cd76398bbafe5
Emmanuel Lagat <lagatemmanuel07@gmail.com>
2026-07-27 16:30:22 +0300

    Add render URL to default ALLOWED_HOSTS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ------------------------------------------------------------------
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,employee-skills-system.onrender.com', cast=Csv())
 
 AUTH_USER_MODEL = 'accounts.User'
 
