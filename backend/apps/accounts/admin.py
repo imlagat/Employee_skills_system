048bee4653c39c9a56b6a0fc538f50e1e9382332
Emmanuel Lagat <lagatemmanuel07@gmail.com>
2026-07-28 12:21:27 +0300

    Configure custom UserAdmin to request email on user creation

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 
 from .models import User
 
 
+class UserCreationForm(BaseUserCreationForm):
+    class Meta(BaseUserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'role', 'first_name', 'last_name')
+
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
+    add_form = UserCreationForm
     list_display = ('username', 'email', 'first_name', 'last_name', 'role', 'is_staff')
     list_filter = BaseUserAdmin.list_filter + ('role',)
     fieldsets = BaseUserAdmin.fieldsets + (
         ('Role', {'fields': ('role',)}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'role', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+
